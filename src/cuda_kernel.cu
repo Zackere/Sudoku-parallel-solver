a3c5429238bcdbcb91c4b32c9fc15412ad9d4096
Zackere <wo1001@wp.pl>
2019-11-21 23:53:54 +0100

    Use shared memory to access old_boards in generator

--- src/cuda_kernel.cu
+++ src/cuda_kernel.cu
@@ -16,7 +16,7 @@ namespace sudoku {
 namespace kernel {
 namespace {
 constexpr unsigned kBlocks = 512;
-constexpr unsigned kThreadsPerBlock = 258;
+constexpr unsigned kThreadsPerBlock = 128;
 constexpr unsigned kIterations = 13;
 
 #define SetNthBit(number, n) ((number) |= (1ul << (n)))
@@ -78,24 +78,38 @@ __global__ void Generator(Board::FieldValue *old_boards, int *old_boards_count,
                           unsigned char *empty_fields_count,
                           Board::FieldValue *solved_board,
                           int *solved_board_mutex) {
-  for (int index = blockIdx.x * blockDim.x + threadIdx.x;
-       index < *old_boards_count; index += blockDim.x * gridDim.x) {
+  __shared__ Board::FieldValue s_current_boards[kThreadsPerBlock *
+                                                Board::kBoardSize *
+                                                Board::kBoardSize];
+  auto *my_board =
+      s_current_boards + threadIdx.x * Board::kBoardSize * Board::kBoardSize;
+  for (int index = blockIdx.x * blockDim.x; index < *old_boards_count;
+       index += blockDim.x * gridDim.x) {
+    __syncthreads();
     if (*solved_board_mutex)
       return;
     for (int i = 0; i < Board::kBoardSize * Board::kBoardSize; ++i) {
-      auto index_mul = index * Board::kBoardSize * Board::kBoardSize;
-      if (!old_boards[i + index_mul]) {
+      auto j = i * kThreadsPerBlock +
+               index * Board::kBoardSize * Board::kBoardSize + threadIdx.x;
+      s_current_boards[i * kThreadsPerBlock + threadIdx.x] =
+          j < *old_boards_count * Board::kBoardSize * Board::kBoardSize
+              ? old_boards[j]
+              : 1;
+    }
+    __syncthreads();
+    for (int i = 0; i < Board::kBoardSize * Board::kBoardSize; ++i) {
+      if (!my_board[i]) {
         auto row = i / Board::kBoardSize;
         auto col = i % Board::kBoardSize;
         for (int j = 1; j <= Board::kBoardSize; ++j) {
-          old_boards[i + index_mul] = j;
-          if (IsValid(old_boards + index_mul, row, col)) {
+          my_board[i] = j;
+          if (IsValid(my_board, row, col)) {
             auto pos = atomicAdd(new_boards_count, 1);
             if (pos < deviceResourceManager::kNBoards) {
               unsigned char empty_index = static_cast<unsigned char>(-1);
               for (int k = 0; k < Board::kBoardSize * Board::kBoardSize; ++k) {
                 if (!(new_boards[pos * Board::kBoardSize * Board::kBoardSize +
-                                 k] = old_boards[index_mul + k]))
+                                 k] = my_board[k]))
                   empty_fields[++empty_index +
                                pos * Board::kBoardSize * Board::kBoardSize] = k;
               }
@@ -109,12 +123,12 @@ __global__ void Generator(Board::FieldValue *old_boards, int *old_boards_count,
         goto NOT_SOLVED;
       }
     }
-    atomicCAS(solved_board_mutex, 0, blockIdx.x * blockDim.x + threadIdx.x);
-    if (*solved_board_mutex != blockIdx.x * blockDim.x + threadIdx.x)
-      return;
-    for (int i = 0; i < Board::kBoardSize * Board::kBoardSize; ++i)
-      solved_board[i] =
-          old_boards[index * Board::kBoardSize * Board::kBoardSize + i];
+    if (threadIdx.x + index < *old_boards_count) {
+      atomicCAS(solved_board_mutex, 0, blockIdx.x * blockDim.x + threadIdx.x);
+      if (*solved_board_mutex == blockIdx.x * blockDim.x + threadIdx.x)
+        for (int i = 0; i < Board::kBoardSize * Board::kBoardSize; ++i)
+          solved_board[i] = my_board[i];
+    }
   NOT_SOLVED:;
   }
 }
